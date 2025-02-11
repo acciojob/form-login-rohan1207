49e39ab410ad1b7eef232db37a13a265a1f0c24e
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-11 18:17:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	
+let name=document.querySelector("#name").value;
+	let lastname=document.querySelector("#last-name").value;
 
+	alert(`${name} ${lastname}`)
 }
